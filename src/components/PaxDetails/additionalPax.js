03ca043ce3369f22651c3d823323d7d7134adcc5
jimmy818 <jmmkaler@gmail.com>
2019-01-11 15:14:39 +0530

    safari issues

--- src/components/PaxDetails/additionalPax.js
+++ src/components/PaxDetails/additionalPax.js
@@ -113,22 +113,25 @@ class AdditionalPax extends Component {
         return (
             <div className={"row mx-0 " +(this.props.isPaxInvalid? "isPaxInvalid":'')}>
             <div className={'col-sm-3 border '+(this.props.it.pax_type==='ADULT' || this.props.it.pax_type==='CHILD' || this.props.it.pax_type==='INFANT'?'':'p-2')}>
-            <label style={{ display:"flex" }} className='mt-2'>
+            
             {/* <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} className="pull-left btnMinus"><i className='fa fa-minus'></i></button> */}
             {
                 this.state.safari?
+                <label style={{ display:"flex" }} className='mt-2'>
                 <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} >A</button>
                 <input type="text" name={this.props.it.pax_type} value={this.state.value} className="additoinalTextBox form-control pull-left" id="noOfRoom" readOnly={true}/>&nbsp;
                 {/* <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} className="btnPlus" ><i className='fa fa-plus'></i></button> */}
                 <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} >M</button>
+                </label>
                 :
+                <label style={{ display:"flex" }} className='mt-2'>
                 <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} >M</button>
                 <input type="text" name={this.props.it.pax_type} value={this.state.value} className="additoinalTextBox form-control pull-left" id="noOfRoom" readOnly={true}/>&nbsp;
                 {/* <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} className="btnPlus" ><i className='fa fa-plus'></i></button> */}
                 <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} >A</button>
+                </label>
             }
             
-            </label>
             </div>
             <div className='col-sm-9 border p-2'>
                 <h5 className='addProductRightHead'>{this.props.it.pax_type} <span className='productAmt'> ({this.props.currency} {this.formatThousands(this.props.it.amount)}) </span></h5>
