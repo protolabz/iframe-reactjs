65bf46d8bda246a6dfe7e4dc048714b936dc5f45
jimmy818 <jmmkaler@gmail.com>
2019-01-11 15:30:43 +0530

    safari issues

--- src/components/PaxDetails/additionalPax.js
+++ src/components/PaxDetails/additionalPax.js
@@ -12,30 +12,8 @@ class AdditionalPax extends Component {
         min:this.props.min,
         max:this.props.max
     }
-
-
-    handleDecreement = (name,data)  => {    
-        this.setState({
-            disablebutton:false
-        })
-        var count = this.state.value;
-        count= count -1;
-        if(data.minimum>0 && count<data.minimum){
-            count = 0;
-            
-        }else{
-            if(count<1){
-                count = 0
-            } 
-        }
-        
-        this.setState({value:count});
-        this.props.decrement(name,data,count);
-        this.props.countValuesDecre();
-    }
-
     handleIncreement = (name,data)  => { 
-        // console.log(data);
+        console.log("ionc");
        let {quota,usedQuota,maxPerBook} =this.state;
        let val1 = quota-usedQuota,val2,maxQutVal=0;
        var count = this.state.value;
@@ -101,6 +79,26 @@ class AdditionalPax extends Component {
             // this.props.countValuesIncre();
     }
 
+    handleDecreement = (name,data)  => { 
+        console.log("Decr");   
+        this.setState({
+            disablebutton:false
+        })
+        var count = this.state.value;
+        count= count -1;
+        if(data.minimum>0 && count<data.minimum){
+            count = 0;
+            
+        }else{
+            if(count<1){
+                count = 0
+            } 
+        }
+        
+        this.setState({value:count});
+        this.props.decrement(name,data,count);
+        this.props.countValuesDecre();
+    }
 
     formatThousands =(n, dp) => {
         var s = ''+(Math.floor(n)), d = n % 1, i = s.length, r = '';
@@ -113,25 +111,13 @@ class AdditionalPax extends Component {
         return (
             <div className={"row mx-0 " +(this.props.isPaxInvalid? "isPaxInvalid":'')}>
             <div className={'col-sm-3 border '+(this.props.it.pax_type==='ADULT' || this.props.it.pax_type==='CHILD' || this.props.it.pax_type==='INFANT'?'':'p-2')}>
-            
+            <p style={{ display:"flex" }} className='mt-2'>
             {/* <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} className="pull-left btnMinus"><i className='fa fa-minus'></i></button> */}
-            {
-                this.state.safari?
-                <label style={{ display:"flex" }} className='mt-2'>
-                <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} >A</button>
-                <input type="text" name={this.props.it.pax_type} value={this.state.value} className="additoinalTextBox form-control pull-left" id="noOfRoom" readOnly={true}/>&nbsp;
-                {/* <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} className="btnPlus" ><i className='fa fa-plus'></i></button> */}
-                <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} >M</button>
-                </label>
-                :
-                <label style={{ display:"flex" }} className='mt-2'>
-                <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} >M</button>
-                <input type="text" name={this.props.it.pax_type} value={this.state.value} className="additoinalTextBox form-control pull-left" id="noOfRoom" readOnly={true}/>&nbsp;
-                {/* <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} className="btnPlus" ><i className='fa fa-plus'></i></button> */}
-                <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} >A</button>
-                </label>
-            }
-            
+            <button id="subs" onClick ={ () => this.handleDecreement(this.props.it.pax_type,this.props.it)} >M</button>
+            <input type="text" name={this.props.it.pax_type} value={this.state.value} className="additoinalTextBox form-control pull-left" id="noOfRoom" readOnly={true}/>&nbsp;
+            {/* <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} className="btnPlus" ><i className='fa fa-plus'></i></button> */}
+            <button type="button" onClick={()=>this.handleIncreement(this.props.it.pax_type,this.props.it)} id="adds" disabled ={this.props.standardPaxDisable} >A</button>
+            </p>
             </div>
             <div className='col-sm-9 border p-2'>
                 <h5 className='addProductRightHead'>{this.props.it.pax_type} <span className='productAmt'> ({this.props.currency} {this.formatThousands(this.props.it.amount)}) </span></h5>
